4d7359e8e8d3aeb07878282c563b75ce39e44fcc
Caio Vinicius <caioviniciuscv18@gmail.com>
2023-05-22 02:10:34 -0300

    Adicionando o primeiro execício de Java pelo Codewars

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,8 @@
+import codewars.EvenOrOdd;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println(EvenOrOdd.even_or_odd(15));
+        System.out.println(EvenOrOdd.even_or_odd(10));
     }
 }
