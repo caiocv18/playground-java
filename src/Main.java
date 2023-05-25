f5fad5a574ea38d396aabf40aa598934790b81f3
Caio Vinicius <caioviniciuscv18@gmail.com>
2023-05-25 01:28:12 -0300

    Adicionando o exercício Dinosaur do MOOC

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,5 @@
-import codewars.EvenOrOdd;
-import codewars.Paper;
-import mooc.AdaLovelace;
-
 public class Main {
     public static void main(String[] args) {
-        int result = Paper.paperWork(5,5);
-        System.out.println(result);
-        int resul2 = Paper.paperWork(0,0);
-        System.out.println(resul2);
-        int resul3 = Paper.paperWork(1,10);
-        System.out.println(resul3);
+
     }
 }
