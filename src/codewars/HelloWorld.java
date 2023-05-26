4c35e1223d5143b708f6a98b923cb27a9b56fcf4
Caio Vinicius <caioviniciuscv18@gmail.com>
2023-05-26 00:21:14 -0300

    Resolvendo o exercício de HelloWorld do Codewars

---
+++ src/codewars/HelloWorld.java
@@ -0,0 +1,14 @@
+package codewars;
+
+/**
+ * Crie uma função simples chamada greet que retorne o mais famoso "hello world!".
+ *
+ * Pontos de estilo
+ * Claro, isso é o mais fácil possível. Mas quão inteligente você pode ser para criar o "hello world" mais criativo que puder imaginar? Qual é a solução de "hello world" que você gostaria de mostrar aos seus amigos?
+ */
+public class HelloWorld {
+    // Write a public static function `greet` that returns "hello world!"
+    public static String greet(){
+        return "hello world!";
+    }
+}
