2d69e8dc6007b13c5a10aeb2cd9597a1e4e883b2
Caio Vinicius <caioviniciuscv18@gmail.com>
2023-05-26 02:00:40 -0300

    Resolvendo o exercício HiAdLovelace do MOOC

---
+++ src/mooc/HiAdaLovelace.java
@@ -0,0 +1,10 @@
+package mooc;
+
+public class HiAdaLovelace {
+
+    public static void main(String[] args) {
+        String name = "Ada Lovelace";
+        System.out.println("Hi " + name + "!");
+
+    }
+}
