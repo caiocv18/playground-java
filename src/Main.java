757a699cd3a40ca6e27ae96c78102be85a67be26
Caio Vinicius <caioviniciuscv18@gmail.com>
2023-06-01 01:55:46 -0300

    Resolvendo o exercício MessageThreeTimes do MOOC

--- src/Main.java
+++ src/Main.java
@@ -2,10 +2,6 @@ import codewars.Alarm;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println(Alarm.setAlarm(true, false));
-        System.out.println(Alarm.setAlarm(true, true));
-        System.out.println(Alarm.setAlarm(false, false));
-        System.out.println(Alarm.setAlarm(false, true));
 
     }
 }
