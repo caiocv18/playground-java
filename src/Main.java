8477065165aa7f610f7e133d154875dea8bfcc45
Caio Vinicius <caioviniciuscv18@gmail.com>
2023-06-01 01:24:48 -0300

    Resolvendo o exercício de Alarm do Codewars

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,11 @@
+import codewars.Alarm;
+
 public class Main {
     public static void main(String[] args) {
+        System.out.println(Alarm.setAlarm(true, false));
+        System.out.println(Alarm.setAlarm(true, true));
+        System.out.println(Alarm.setAlarm(false, false));
+        System.out.println(Alarm.setAlarm(false, true));
 
     }
 }
---
+++ src/codewars/Alarm.java
@@ -0,0 +1,9 @@
+package codewars;
+
+public class Alarm {
+
+    public static boolean setAlarm(boolean employed, boolean vacation) {
+        return employed && !vacation;
+    }
+
+}
