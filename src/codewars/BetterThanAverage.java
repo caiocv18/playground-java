8985e15e88ad383ef87c037d34246ccbbf9b5768
Caio Vinicius <caioviniciuscv18@gmail.com>
2023-06-26 23:42:24 -0300

    Refactoring the class name

--- src/codewars/BetterThanAverage.java
+++ src/codewars/BetterThanAverage.java
@@ -2,7 +2,7 @@ package codewars;
 
 import java.util.Arrays;
 
-public class Kata {
+public class BetterThanAverage {
     public static boolean betterThanAverage(int[] classPoints, int yourPoints) {
         float average = (float) Arrays.stream(classPoints).sum() / classPoints.length;
         return yourPoints > average;
